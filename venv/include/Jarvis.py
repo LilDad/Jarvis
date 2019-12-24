5f8b5e58697535debc139a7a2c5f9f123d1fd1a7
LilDaddy <bezhuraedgar@gmail.com>
2019-12-24 09:57:31 +0200

    Env Init

---
+++ venv/include/Jarvis.py
@@ -0,0 +1,13 @@
+import os
+import time
+# import speech_recognition as sr
+# import fuzzywuzzy as fuzz
+
+# преобразование текста в речь
+import pyttsx3
+import datetime
+
+engine = pyttsx3.init()
+engine.say("Hey you!")
+engine.runAndWait()
+
