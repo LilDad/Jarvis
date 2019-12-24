078d30bf529137d9d829660e5a1dfd3a60aa9c46
LilDaddy <bezhuraedgar@gmail.com>
2019-12-24 10:19:54 +0200

    Micro Index

--- venv/include/Jarvis.py
+++ venv/include/Jarvis.py
@@ -6,7 +6,10 @@ import fuzzywuzzy as fuzz
 import pyttsx3
 import datetime
 
-engine = pyttsx3.init()
-engine.say("Hey you! I am Jarvis")
-engine.runAndWait()
+# engine = pyttsx3.init()
+# engine.say("Hey you! I am Jarvis")
+# engine.runAndWait()
 
+# определение индекса микрофона
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
