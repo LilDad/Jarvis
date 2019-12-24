32f8477521a34204fa16cf62962aeca1b2d25b83
LilDaddy <bezhuraedgar@gmail.com>
2019-12-24 10:04:21 +0200

    Env update

--- venv/include/Jarvis.py
+++ venv/include/Jarvis.py
@@ -1,13 +1,12 @@
 import os
 import time
-# import speech_recognition as sr
-# import fuzzywuzzy as fuzz
-
+import speech_recognition as sr
+import fuzzywuzzy as fuzz
 # преобразование текста в речь
 import pyttsx3
 import datetime
 
 engine = pyttsx3.init()
-engine.say("Hey you!")
+engine.say("Hey you! I am Jarvis")
 engine.runAndWait()
 
