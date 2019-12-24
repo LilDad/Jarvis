aac57d388ed6aba36765e863329ec6d7820e563b
LilDaddy <bezhuraedgar@gmail.com>
2019-12-24 11:20:14 +0200

    opts add

--- venv/include/Jarvis.py
+++ venv/include/Jarvis.py
@@ -1,6 +1,8 @@
 import os
 import time
+# Google API для распознование речи
 import speech_recognition as sr
+# модуль для нечеткого сравнения
 import fuzzywuzzy as fuzz
 # преобразование текста в речь
 import pyttsx3
@@ -11,5 +13,20 @@ import datetime
 # engine.runAndWait()
 
 # определение индекса микрофона
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+# for index, name in enumerate(sr.Microphone.list_microphone_names()):
+#     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
+opts = {
+    "alias": ('джарвис', 'джар', 'джа-джа', 'джо'),
+    "tbr": (),
+    "cmds": ()
+}
+
+r = sr.Recognizer()
+
+with sr.Microphone(device_index=0) as source:
+    print("Say some thing...")
+    audio = r.listen(source)
+
+query = r.recognize_google(audio, language="ru-RU")
+print("Ты сказал: " + query.lower())
